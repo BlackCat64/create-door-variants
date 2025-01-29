690e4c32541a115a4a0914d35747b0dbeeb12ee5
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-01-29 14:59:31 +0000

    Added model and blockstate files for White Train Door

--- src/main/java/net/blackcat64/createdoorvariants/block/ModBlocks.java
+++ src/main/java/net/blackcat64/createdoorvariants/block/ModBlocks.java
@@ -2,11 +2,17 @@ package net.blackcat64.createdoorvariants.block;
 
 import com.simibubi.create.AllBlocks;
 import com.simibubi.create.content.decoration.slidingDoor.SlidingDoorBlock;
+import com.simibubi.create.foundation.data.BuilderTransformers;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.tterrag.registrate.util.entry.BlockEntry;
 import net.blackcat64.createdoorvariants.CreateDoorVariantsMod;
 import net.blackcat64.createdoorvariants.item.ModItems;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -16,16 +22,23 @@ import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Supplier;
 
+import static com.simibubi.create.Create.REGISTRATE;
+
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, CreateDoorVariantsMod.MODID);
 
+    public static final CreateRegistrate MOD_REGISTRATE = CreateRegistrate.create(CreateDoorVariantsMod.MODID)
+            .defaultCreativeTab((ResourceKey<CreativeModeTab>) null);
 
-    public static final RegistryObject<SlidingDoorBlock> WHITE_TRAIN_DOOR =
-            registerBlock("white_train_door", () -> SlidingDoorBlock.metal(BlockBehaviour.Properties
-                    .copy(AllBlocks.TRAIN_DOOR.get())
-                    .mapColor(MapColor.TERRACOTTA_WHITE), false));
+    public static final BlockEntry<SlidingDoorBlock> WHITE_TRAIN_DOOR =
+            REGISTRATE.block("white_train_door", p -> SlidingDoorBlock.metal(p, false))
+                    .transform(BuilderTransformers.slidingDoor("white_train"))
+                    .properties(p -> p.mapColor(MapColor.TERRACOTTA_CYAN)
+                            .sound(SoundType.NETHERITE_BLOCK)
+                            .noOcclusion())
+                    .register();
 
 
     public static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
